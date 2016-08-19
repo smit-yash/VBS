98b54ef2f8938da6fcce7c3348237c37125c5c00
Smit Yash <smit.yash@optimusinfo.com>
2016-08-20 01:05:35 +0530

    Initial Files

---
+++ Source/Balco/Balco/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Balco
+//
+//  Created by optimusmac-12 on 20/08/16.
+//  Copyright © 2016 sy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Source/Balco/Balco/HomeViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Balco
+//
+//  Created by optimusmac-12 on 20/08/16.
+//  Copyright © 2016 sy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
