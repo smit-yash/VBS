78341b43454587c7d0539e22962afe39a3375b41
Smit Yash <smit.yash@optimusinfo.com>
2016-08-20 01:31:58 +0530

    Initial files

---
+++ Source/Balco/Balco/MVC/Controller/HomeViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Balco
+//
+//  Created by optimusmac-12 on 20/08/16.
+//  Copyright © 2016 sy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
